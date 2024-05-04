6b6880fae84065c28ee7118a57d4e2148ddf13f3
Konano <nanoapezlk@gmail.com>
2024-05-04 17:20:11 +0800

    add build.sh

---
+++ build-aarch64.sh
@@ -0,0 +1,2 @@
+docker build -t nanoape/docker-openvpn:aarch64 -f Dockerfile.arm64 .
+docker push nanoape/docker-openvpn:aarch64
---
+++ build-x86_64.sh
@@ -0,0 +1,2 @@
+docker build -t nanoape/docker-openvpn:x86_64 .
+docker push nanoape/docker-openvpn:x86_64
